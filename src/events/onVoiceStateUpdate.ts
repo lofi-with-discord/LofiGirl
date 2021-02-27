707dbe07406a51257187ee28487c16fb42d7e54f
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-02-28 01:27:59 +0900

    revert of e2c0320

--- src/events/onVoiceStateUpdate.ts
+++ src/events/onVoiceStateUpdate.ts
@@ -1,31 +1,30 @@
-import { VoiceChannel, VoiceState } from 'discord.js'
+import { VoiceState } from 'discord.js'
 import Client from '../classes/Client'
 
-export default async function onVoiceStateUpdate (client: Client, oldState: VoiceState, newState: VoiceState) {
-  if (oldState.member?.user.bot) return
-  if (newState.member?.user.bot) return
-
+export default async function onVoiceStateUpdate (client: Client, old: VoiceState, state: VoiceState) {
   const channels = await client.db.select('*').from('channels')
 
   for (const channel of channels) {
-    const rawChannel = client.channels.resolve(channel.id)
-    if (!rawChannel) continue
+    if (old.channelID === channel.id && state.channelID !== channel.id) {
+      if (!old.channel) return
 
-    const voiceChannel = rawChannel as VoiceChannel
-    const joinMembers = voiceChannel.members.filter((m) => !m.user.bot).size
-
-    if (joinMembers < 1) {
-      client.lavalink.leave(voiceChannel.guild.id)
-      continue
+      if (old.channel.members.filter((member) => !member.user.bot).size < 1) {
+        client.lavalink.leave(old.guild.id)
+        continue
+      }
     }
 
-    const player = client.lavalink.players.get(voiceChannel.guild.id)
+    if (old.channelID !== channel.id && state.channelID === channel.id) {
+      if (!state.channel) return
 
-    if (!player?.playing) {
-      const { themeId = 1 } = ((await client.db.select('theme').where({ guild: voiceChannel.guild.id }).from('channels'))[0] || {})
-      const [themeData] = await client.db.select('*').from('themes').where({ id: themeId || 1 })
-
-      client.lavalink.play(voiceChannel, themeData.url)
+      const { theme = 0 } = ((await client.db.select('theme').where({ guild: state.guild.id }).from('channels'))[0] || {})
+      const [themeData] = await client.db.select('*').from('themes').where({ id: theme })
+      client.lavalink.play(state.channel, themeData.url)
     }
+  }
+
+  if (!old?.channel?.members) return
+  if (old.channel.members.filter((member) => !member.user.bot).size < 1) {
+    client.lavalink.leave(old.guild.id)
   }
 }
