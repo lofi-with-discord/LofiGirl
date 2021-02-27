e2c032079cb1696ba63f5f0e34e1ecda86dd418a
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-02-28 01:00:43 +0900

    fix: VoiceStateUpdate

--- src/events/onVoiceStateUpdate.ts
+++ src/events/onVoiceStateUpdate.ts
@@ -1,30 +1,27 @@
-import { VoiceState } from 'discord.js'
+import { VoiceChannel, VoiceState } from 'discord.js'
 import Client from '../classes/Client'
 
-export default async function onVoiceStateUpdate (client: Client, old: VoiceState, state: VoiceState) {
+export default async function onVoiceStateUpdate (client: Client, oldState: VoiceState, newState: VoiceState) {
+  if (oldState.member?.user.bot) return
+  if (newState.member?.user.bot) return
+
   const channels = await client.db.select('*').from('channels')
 
   for (const channel of channels) {
-    if (old.channelID === channel.id && state.channelID !== channel.id) {
-      if (!old.channel) return
+    const rawChannel = client.channels.resolve(channel.id)
+    if (!rawChannel) continue
 
-      if (old.channel.members.filter((member) => !member.user.bot).size < 1) {
-        client.lavalink.leave(old.guild.id)
-        continue
-      }
+    const voiceChannel = rawChannel as VoiceChannel
+    const joinMembers = voiceChannel.members.filter((m) => !m.user.bot).size
+
+    if (joinMembers < 1) {
+      client.lavalink.leave(voiceChannel.guild.id)
+      continue
     }
 
-    if (old.channelID !== channel.id && state.channelID === channel.id) {
-      if (!state.channel) return
-
-      const { theme = 0 } = ((await client.db.select('theme').where({ guild: state.guild.id }).from('channels'))[0] || {})
-      const [themeData] = await client.db.select('*').from('themes').where({ id: theme })
-      client.lavalink.play(state.channel, themeData.url)
+    if (!client.lavalink.players.has(voiceChannel.guild.id)) {
+      const [themeData] = await client.db.select('*').from('themes').where({ id: channel.theme })
+      client.lavalink.play(voiceChannel, themeData.url)
     }
-  }
-
-  if (!old?.channel?.members) return
-  if (old.channel.members.filter((member) => !member.user.bot).size < 1) {
-    client.lavalink.leave(old.guild.id)
   }
 }
